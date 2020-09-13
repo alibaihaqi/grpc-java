8a37d9b622ddf6015991659289dd33d0e2443fe2
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2020-09-14 01:18:57 +0700

    refactor: refactor calculator client java fille

--- src/main/java/com/alibaihaqi/simplesteph/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/alibaihaqi/simplesteph/grpc/calculator/client/CalculatorClient.java
@@ -6,30 +6,41 @@ import io.grpc.ManagedChannelBuilder;
 
 public class CalculatorClient {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello, I'm a gRPC Client");
-
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052)
                 .usePlaintext()
                 .build();
 
         System.out.println("Creating stub!");
 
+        doUnaryCall(channel);
+        doServerStreamingCall(channel);
+
+        // do something
+        System.out.println("Shutting down channel!");
+        channel.shutdown();
+    }
+
+    private void doUnaryCall (ManagedChannel channel) {
         // created a calculator service client (blocking - synchronous)
         CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient = CalculatorServiceGrpc.newBlockingStub(channel);
 
         // Unary
-//        // do the same for a CalculatorRequest
-//        SumRequest request = SumRequest.newBuilder()
-//                .setFirstNumber(3)
-//                .setSecondNumber(10)
-//                .build();
-//
-//        // Call the RPC and get back a CalculatorResponse (protocol buffers)
-//        SumResponse response = calculatorClient.sum(request);
-//
-//        System.out.println("response calculator: " +  response);
+        // do the same for a CalculatorRequest
+        SumRequest request = SumRequest.newBuilder()
+                .setFirstNumber(3)
+                .setSecondNumber(10)
+                .build();
+
+        // Call the RPC and get back a CalculatorResponse (protocol buffers)
+        SumResponse response = calculatorClient.sum(request);
+
+        System.out.println("response calculator: " +  response);
+    }
+
+    private void doServerStreamingCall (ManagedChannel channel) {
+        // created a calculator service client (blocking - synchronous)
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient = CalculatorServiceGrpc.newBlockingStub(channel);
 
         // Server Streaming
         PrimeNumberDecompositionRequest request = PrimeNumberDecompositionRequest.newBuilder()
@@ -41,9 +52,12 @@ public class CalculatorClient {
                 .forEachRemaining(primeNumberDecompositionResponse -> {
                     System.out.println("prime number decomposition value: " + primeNumberDecompositionResponse.getResultNumber());
                 });
+    }
 
-        // do something
-        System.out.println("Shutting down channel!");
-        channel.shutdown();
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm a gRPC Client");
+
+        CalculatorClient main = new CalculatorClient();
+        main.run();
     }
 }
